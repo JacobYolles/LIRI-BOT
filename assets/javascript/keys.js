d90c8dcc6cb953233aac39dcbbad263a7c315238
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-25 17:32:42 -0600

    small tweeks

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,6 +1,10 @@
+// This page is controlling all the exported keys for the different devices that are running.  AKA SPOTIFY ETC. 
+
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: bc3af9e2e6df455b980dcfb99e40eca9,
+  secret: a1f171a0c3443bda2daae2e6b24cda7
 };
+
+
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,8 +1,18 @@
+// This page will be for the actual "Liri" App to run.
+
 require("dotenv").config();
+
+// Creating all requires:
+// var inquirer = require("inquirer");
+var spotify = require('spotify');  // Spotify
+var request = require('request');  // Movies
+var fs = require('fs'); // File Structure
+var keys = require("./keys.js");    // Keys file for all consumer and secret keys.
+
 
 var spotify = new Spotify(keys.spotify);
 
-//  make the following work
+// Known variables to make work:
 // concert-this
 // spotify-this-song
 // movie-this
