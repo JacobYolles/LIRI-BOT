da6b8ca72e294edacb1973fea8a2e25ad9eacb2b
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-26 16:33:12 -0600

    commit

--- liri.js
+++ liri.js
@@ -4,9 +4,9 @@
 
 require("dotenv").config();
 // var inquirer = require("inquirer");
-var spotify = require('spotify');  // Spotify
-var request = require('request');  // Movies
-var fs = require('fs'); // File Structure
+var spotify = require("spotify");  // Spotify
+var request = require("request");  // Movies
+var fs = require("fs"); // File Structure
 var keys = require("./keys.js");    // Keys file for all consumer and secret keys.
 
 
@@ -17,6 +17,7 @@ var inputCommand = process.argv[2]; // This is the switch function to initate wh
 var commandParam = process.arv[3];  // This is the specific function or song part that the user wants after they are inside the application.
 var defaultMovie = "Ghostbusters"
 var defaultSong = "Kiss from a rose"
+var defaultConcert = "Neil Diamond"
 var spotify = new Spotify(keys.spotify);
 
 // var undefined = undefined
@@ -38,7 +39,14 @@ var spotify = new Spotify(keys.spotify);
 function processCommands(command, commandParam)
 
 // console.log(commandParam);
-switch (command) {
+switch(command){
+    // Run the concert this functionality
+    case "concert-this":
+    //if the user does not pick a concert, use my var for default concert.
+    if (commandParam === undefined) {
+        commandParam === defaultConcert;
+    }
+
 
     // Run the spotify this song functionality.
     case "spotify-this-song":
@@ -75,9 +83,9 @@ function spotifyThisSong(song) {
         song = "Kiss from a rose";
     } // end tag for the first if.
 
-    spotify.search({ type: 'track', query: song}, function(err, data) {
+    spotify.search({ type: "track", query: song}, function(err, data) {
         if (err) {
-            console.log('Error occurred: ' + err);
+            console.log("Error occurred: " + err);
             return;
         }
     
@@ -100,7 +108,78 @@ function spotifyThisSong(song) {
 
 }// end tag for the function spotifythis song command.
 
+//Create the movie command
+function movieThis(movieName){
 
+	console.log(movieName);
+
+	request("https://api.themoviedb.org/3/search/movie?api_key=trilogy" + tmdbKey + "&query=" + movieName, function(error, response, body) {
+
+  	// If there were no errors and the response code was 200 (i.e. the request was successful)...
+  	if (!error && response.statusCode === 200) {
+
+	    //console.log(JSON.parse(body));
+	    
+	    //Get the Movie ID
+	    var movieID =  JSON.parse(body).results[0].id;
+	    //console.log(movieID);
+
+	    //Create new query using the movie ID
+	    var queryURL = "https://api.themoviedb.org/3/movie/" + movieID + "?api_key=trilogy" + tmdbKey + "&append_to_response=credits,releases";
+
+	    request(queryURL, function(error, response, body) {
+	    	var movieObj = JSON.parse(body);
+
+	    	console.log("--------Title-----------");
+	    	console.log(movieObj.original_title);
+
+	    	console.log("--------Year -----------");
+	    	console.log(movieObj.release_date.substring(0,4));
+
+	   		console.log("--------Rating-----------");
+	   		console.log(movieObj.releases.countries[0].certification);
+
+	   		console.log("--------Country Produced-----------");
+	   		for(i=0, j = movieObj.production_countries.length; i<j; i++){
+	   			console.log(movieObj.production_countries[i].name);
+	   		}
+	   		console.log("--------Languages-----------");
+	   		for(i=0, j = movieObj.spoken_languages.length; i<j; i++){
+	   			console.log(movieObj.spoken_languages[i].name);
+	   		}
+	   		console.log("--------Plot----------------");
+	   		console.log(movieObj.overview);
+
+	   		console.log("--------Actors-----------");
+	   		for(i=0, j = movieObj.credits.cast.length; i<j; i++){
+	   			console.log(movieObj.credits.cast[i].name);
+	   		}
+	    	
+	    });
+
+
+  	}else{
+  		console.log(error);
+  	}
+
+	});
+}
+
+
+
+// Create the do what it says command.
+function doWhatItSays(){
+	fs.readFile("random.txt", "utf8", function(err, data){
+
+		if (err){ 
+			return console.log(err);
+		}
+
+		var dataArr = data.split(",");
+
+		processCommands(dataArr[0], dataArr[1]);
+	});
+}
 
 /////////////////////////////////////////////////////////////////// Main run Callback Function ////////////////////////////////////////////////////
 
